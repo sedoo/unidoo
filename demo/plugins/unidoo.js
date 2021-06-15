ff756878b0544c69e7fe14c5126d4c3eb5e26faf
Waxoussou <45203020+Waxoussou@users.noreply.github.com>
2021-06-15 07:40:54 +0000

    Deploying to gh-pages from @ sedoo/unidoo@cb207b5e61d61d3b63761c5d3dd1f9c00195b64d 🚀

--- demo/plugins/unidoo.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue"
-import Unidoo from "@/plugin"
-/* point to /src/plugin.js for dev purpose
-in production replace with: import VuetifyExtra from '@menteora/vuetify-extra' and
-import '@menteora/vuetify-extra/dist/menteora.css' */
-
-Vue.use(Unidoo)
