620d08c6c6f0df0995bf33c933663bd6a4d02785
Francois ANDRE <francois.andre@obs-mip.fr>
2021-06-25 17:05:35 +0200

    Minor fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'by Sedoo',
     base: '/unidoo/',
     themeConfig: {
-    	logo: '/assets/img/unidoo.png',
+    	logo: '/public/img/unidoo.png',
         nav: [
             { text: 'root', link: '/' },
             // { text: 'components', items: [{ text: 'Unidoo Alert', link: '/unidoo-alert/' }, { text: 'modal de confirmation', link: '/confirm-dialog/' }] },
