09ca7245a9c40b9354dad11c9342d2e9749eed5a
andry <andry.andriatiana@obs-mip.fr>
2021-10-26 15:10:20 +0200

    add a slot to includ custom fields in slider part

--- docs/.vuepress/components/DemoUnidooPlayer.vue
+++ docs/.vuepress/components/DemoUnidooPlayer.vue
@@ -1,11 +1,42 @@
 <template>
     <v-app>
         <v-col>
+            <v-switch v-model="showCustom" label="Afficher custom field"></v-switch>
             <v-text-field label="label" v-model="label"/>
             <unidoo-player
               :label="label"
               :data="values"
-            ></unidoo-player>
+            >
+                <template v-if="showCustom" v-slot:customField>
+                    
+                    <v-menu
+                        bottom
+                        right
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            class="mx-2"
+                            fab
+                            x-small
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            <v-icon>mdi-dots-horizontal</v-icon>
+                        </v-btn>
+                        </template>
+
+                        <v-list>
+                        <v-list-item
+                            v-for="(item, i) in items"
+                            :key="i"
+                        >
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                        </v-list-item>
+                        </v-list>
+                    </v-menu>
+                
+                </template>
+            </unidoo-player>
             <a href="https://misva.aeris-data.fr/">* Images provenant du site web MISVA</a>
         </v-col>
     </v-app>
@@ -16,7 +47,13 @@ export default {
     data() {
         return {
             label: '2021-09-01',
+            showCustom: false,
             values: null,
+            items: [
+                { title: 'Click Me' },
+                { title: 'Click Me' },
+                { title: 'Click Me' },
+            ]
         };
     },
     mounted() {
